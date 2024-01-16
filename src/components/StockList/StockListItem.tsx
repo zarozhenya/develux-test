57352ce9da2ff3c9a5f3cda115731ead471f3ed0
Yevhen <eug.z2004@gmail.com>
2024-01-16 11:32:38 +0200

    added stock item component

--- src/components/StockList/StockListItem.tsx
+++ src/components/StockList/StockListItem.tsx
@@ -1,11 +1,24 @@
 import React, {FC} from 'react';
+import {Text, View} from 'react-native';
 import {StockItem} from '../../types';
-import {Text} from 'react-native';
+import {styles} from './styles';
 
 interface StockListItemProps {
   item: StockItem;
 }
 
-export const StockListItem: FC<StockListItemProps> = ({item}) => {
-  return <Text>{item.market}</Text>;
+export const StockListItem: FC<StockListItemProps> = ({item: {i, market}}) => {
+  const price = i.price.lastTradedPrevious * Number(i.lotSize);
+  return (
+    <View style={styles.itemContainer}>
+      <Text style={styles.itemText}>
+        {i.name}({i.type}) - {market}
+      </Text>
+      <View style={styles.itemPriceContainer}>
+        <Text style={styles.itemPriceText}>
+          {Number((price / 100).toFixed(2))}
+        </Text>
+      </View>
+    </View>
+  );
 };
---
+++ src/components/StockList/styles.ts
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+  },
+  itemText: {
+    fontSize: 17,
+    lineHeight: 22,
+  },
+  itemPriceContainer: {
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    backgroundColor: '#5DD25A',
+  },
+  itemPriceText: {
+    fontSize: 13,
+    lineHeight: 22,
+    color: '#ffffff',
+  },
+});
