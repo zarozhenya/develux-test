ad91cc788031a2663d91c34468740fe4a8586e77
Yevhen <eug.z2004@gmail.com>
2024-01-16 10:08:18 +0200

    added svg transformation and some icons

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {SearchScreen, StocksScreen} from '../screens';
+import StocksIcon from '../assets/stock-icon.svg';
 
 const Tab = createBottomTabNavigator();
 
 export const MainStack: React.FC = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Stocks" component={StocksScreen} />
+      <Tab.Screen
+        name="Stocks"
+        component={StocksScreen}
+        options={{
+          tabBarIcon: ({color}) => <StocksIcon stroke={color} />,
+        }}
+      />
       <Tab.Screen name="Search" component={SearchScreen} />
     </Tab.Navigator>
   );
